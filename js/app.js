7683deef9f61a130512a9768b8867e2f4aa0ae7b
jgudhlanga <james@propay.co.za>
2018-10-16 14:56:45 +0200

    implemented the attack method

--- js/app.js
+++ js/app.js
@@ -6,9 +6,56 @@ new Vue({
         isGameRunning: false
     },
     methods: {
-        startGame: function () {
+        startGame() {
             this.isGameRunning = true;
             this.playerHealth = 100;
-        }
+            this.monsterHealth = 100;
+        },
+        attack() {
+
+            this.monsterHealth -= this.calculateDamage(3, 10);
+            if (this.checkWin()){
+                return;
+            }
+
+            this.playerHealth -= this.calculateDamage(5, 12);
+            this.checkWin();
+        },
+        
+        specialAttack() {
+            
+        },
+        
+        heal() {
+            
+        },
+        
+        giveUp() {
+            
+        },
+
+        calculateDamage(min, max) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+
+         checkWin() {
+             if(this.monsterHealth <= 0) {
+                 if (confirm('You won! New Game?')){
+                     this.startGame();
+                 }
+                 else{
+                     this.isGameRunning = false;
+                 }
+                 return true;
+             } else if (this.playerHealth <= 0) {
+                 if (confirm('You lost! New Game?')) {
+                     this.startGame();
+                 } else {
+                     this.isGameRunning = false;
+                 }
+                 return true;
+             }
+             return false;
+         }
     }
 });
